76143d1d077c2e3e06773523232ce6dca7ecff2c
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-10-04 11:56:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(json => {
+        setData(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {loading ? (
+        <span>Loading...</span>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
