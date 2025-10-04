2b48fbd30d9bbbf1c33522438207438e24a406c2
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-10-04 12:04:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,21 @@ import './../styles/App.css';
 const App = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     fetch('https://dummyjson.com/products')
       .then(response => response.json())
       .then(json => {
-        setData(json);
+        if (!json || Object.keys(json).length === 0) {
+          setData(null);
+        } else {
+          setData(json);
+        }
+        setLoading(false);
+      })
+      .catch(() => {
+        setError("Error fetching data");
         setLoading(false);
       });
   }, []);
@@ -19,11 +28,18 @@ const App = () => {
       {/* Do not remove the main div */}
       {loading ? (
         <span>Loading...</span>
+      ) : error ? (
+        <span>{error}</span>
+      ) : !data ? (
+        <span>No data found</span>
       ) : (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <>
+          <span>Data Fetched from API</span>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </>
       )}
     </div>
   );
-}
+};
 
 export default App;
